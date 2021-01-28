0ffb32d9edfece61b66ab3211029bb41adce4803
Luiz Rosa <LuizRosa@NCEI-6164.centropaulasouza.sp.gov.br>
2021-01-28 19:42:43 -0300

    FindById Implementation

--- src/application/ProgMain.java
+++ src/application/ProgMain.java
@@ -1,22 +1,24 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class ProgMain {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Livros");
-		
-		Seller seller = new Seller(21, "Luiz", "luiz@email.com", new Date(), 7000.0, obj);
-		
+		/*
+		 * Department obj = new Department(1, "Livros");
+		 * 
+		 * Seller seller = new Seller(21, "Luiz", "luiz@email.com", new Date(), 7000.0,
+		 * obj);
+		 */
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
+		Seller seller = sellerDao.findById(3);
+
 		System.out.println(seller);
 	}
 
