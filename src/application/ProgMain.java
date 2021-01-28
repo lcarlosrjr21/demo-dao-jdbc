0a5c590ef53c0859a800fe8832902db8ec045a18
Luiz Rosa <LuizRosa@NCEI-6164.centropaulasouza.sp.gov.br>
2021-01-28 19:18:05 -0300

    SellerDaoJDBC e DaoFactory

--- src/application/ProgMain.java
+++ src/application/ProgMain.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class ProgMain {
 		Department obj = new Department(1, "Livros");
 		
 		Seller seller = new Seller(21, "Luiz", "luiz@email.com", new Date(), 7000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 	}
